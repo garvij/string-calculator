bb8614403994c42aa2983b4c5e35e96e25f2c305
Vijay <t-vijay@talasecurity.io>
2021-01-29 21:32:35 +0530

    initial tests

---
+++ src/main/java/com/tdd/kata/stringcalculator/StringCalculator.java
@@ -0,0 +1,8 @@
+package com.tdd.kata.stringcalculator;
+
+public class StringCalculator {
+
+    public int add(String numbers){
+        return 0;
+    }
+}
---
+++ src/test/java/com/tdd/kata/stringcalculator/StringCalculatorTest.java
@@ -0,0 +1,60 @@
+package com.tdd.kata.stringcalculator;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class StringCalculatorTest {
+
+    private StringCalculator stringCalculator;
+
+    /**
+     * TODO :-
+     * link: https://osherove.com/tdd-kata-1
+     *
+     * The method can take up to two numbers, separated by commas, and will return their sum.
+     * for example “” or “1” or “1,2” as inputs.
+     * (for an empty string it will return 0)
+     * Hints:
+     * ——————
+     * - Start with the simplest test case of an empty string and move to one and two numbers
+     * - Remember to solve things as simply as possible so that you force yourself
+     * to write tests you did not think about
+     * - Remember to refactor after each passing test
+     */
+
+    @BeforeEach
+    void before() {
+        stringCalculator = new StringCalculator();
+    }
+
+    @Test
+    void whenEmptyString_ShouldReturnZero() {
+        stringCalculator = new StringCalculator();
+        assertEquals(0, stringCalculator.add(""));
+    }
+
+    // TODO : fix for below tests
+    @Test
+    void return2() {
+        stringCalculator = new StringCalculator();
+        assertEquals(2, stringCalculator.add("2"));
+    }
+
+    @Test
+    void return3() {
+        stringCalculator = new StringCalculator();
+        assertEquals(3, stringCalculator.add("3"));
+    }
+
+    @Test
+    void return5() {
+        stringCalculator = new StringCalculator();
+        assertEquals(5, stringCalculator.add("2,3"));
+        assertEquals(5, stringCalculator.add("3,2"));
+    }
+
+
+}
