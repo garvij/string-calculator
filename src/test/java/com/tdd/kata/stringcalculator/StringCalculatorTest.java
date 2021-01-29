f7e6644005e50a5554c36a12ac27e1f173b4ee45
Vijay <t-vijay@talasecurity.io>
2021-01-29 21:46:03 +0530

    Allow the Add method to handle an unknown amount of numbers

--- src/test/java/com/tdd/kata/stringcalculator/StringCalculatorTest.java
+++ src/test/java/com/tdd/kata/stringcalculator/StringCalculatorTest.java
@@ -12,7 +12,7 @@ class StringCalculatorTest {
 
     /**
      * link: https://osherove.com/tdd-kata-1
-     *
+     * <p>
      * The method can take up to two numbers, separated by commas, and will return their sum.
      * for example “” or “1” or “1,2” as inputs.
      * (for an empty string it will return 0)
@@ -31,28 +31,46 @@ class StringCalculatorTest {
 
     @Test
     void whenEmptyString_ShouldReturnZero() {
-        stringCalculator = new StringCalculator();
         assertEquals(0, stringCalculator.add(""));
     }
 
     @Test
-    void return2() {
-        stringCalculator = new StringCalculator();
+    void return_2() {
         assertEquals(2, stringCalculator.add("2"));
     }
 
     @Test
-    void return3() {
-        stringCalculator = new StringCalculator();
+    void return_3() {
         assertEquals(3, stringCalculator.add("3"));
     }
 
     @Test
-    void return5() {
-        stringCalculator = new StringCalculator();
+    void return_5() {
         assertEquals(5, stringCalculator.add("2,3"));
         assertEquals(5, stringCalculator.add("3,2"));
     }
+
+    /**
+     * 2. Allow the Add method to handle an unknown amount of numbers
+     */
+    @Test
+    void return_7() {
+        assertEquals(7, stringCalculator.add("2,3,2"));
+        assertEquals(7, stringCalculator.add("3,2,2"));
+    }
+
+    @Test
+    void return_9() {
+        assertEquals(9, stringCalculator.add("1,2,3,3"));
+        assertEquals(9, stringCalculator.add("3,2,3,1"));
+    }
+
+    /**
+     * TODO:
+     * Allow the Add method to handle new lines between numbers (instead of commas).
+     * the following input is ok: “1\n2,3” (will equal 6)
+     * the following input is NOT ok: “1,\n” (not need to prove it - just clarifying)
+     */
 
 
 }
