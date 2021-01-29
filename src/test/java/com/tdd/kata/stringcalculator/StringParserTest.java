1fe66b9e9f3103edfc8c666dc1bda3e997660965
Vijay <t-vijay@talasecurity.io>
2021-01-29 22:21:48 +0530

    string parser tests

---
+++ src/test/java/com/tdd/kata/stringcalculator/StringParserTest.java
@@ -0,0 +1,28 @@
+package com.tdd.kata.stringcalculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringParserTest {
+
+    private StringParser stringParser;
+
+    @BeforeEach
+    void before() {
+        stringParser = new StringParser();
+    }
+
+    @Test
+    void test_provided_delimiter_parsing() {
+        assertEquals(Arrays.asList(5, 5), stringParser.parse("//;\n5;5"));
+        assertEquals(Arrays.asList(2, 3, 5), stringParser.parse("//;\n2;3;5"));
+        assertEquals(Arrays.asList(5, 5), stringParser.parse("//,\n5,5"));
+        assertEquals(Arrays.asList(2, 3, 5), stringParser.parse("//,\n2,3,5"));
+    }
+
+
+}
